8a104d2e5192a82860d578fce5d7f028f6bc5f39
Sashke <hellosashke@gmail.com>
2014-11-30 20:56:38 +0600

    Initial Commit

---
+++ SoundCloudStatusBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SoundCloudStatusBar
+//
+//  Created by Sashke on 30.11.14.
+//  Copyright (c) 2014 Alexander Rubin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SoundCloudStatusBar/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SoundCloudStatusBar
+//
+//  Created by Sashke on 30.11.14.
+//  Copyright (c) 2014 Alexander Rubin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
